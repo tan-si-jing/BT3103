650cc11ef830f617797b82dba13a4aa38c315ecc
tan-si-jing <65453969+tan-si-jing@users.noreply.github.com>
2021-04-19 02:43:44 +0800

    Add files via upload

---
+++ src/components/Company/EditProfile.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+  <div class="editProfile" style="margin: 100px;">    
+    <div class="intro h-100">
+      <div class="h-100 align-items-center">
+        <h3 style="margin: 40px;">Account settings</h3>
+      </div>
+    </div>
+    <div class="profile-content">
+      <div class="container">
+
+          <div class="col-sm">
+            <div class="form-group">
+              <input type="text"  v-model="email" :placeholder="this.account.email" class="form-control">
+            </div>
+          </div>
+          
+          <div class="col-sm">
+            <div class="form-group" style="text-align: right;">
+              <input type="submit" v-on:click="updateProfile" value="Save Changes" class="btn btn-primary">
+            </div>
+          </div>
+          <br>
+
+          <div class="col-sm">
+            <div class="form-group">
+              <input type="password" v-model="password" placeholder="New password" class="form-control">
+            </div>
+          </div>
+
+          <div class="col-sm">
+            <div class="form-group">
+              <input type="password" v-model="confirmPassword" placeholder="Confirm password" class="form-control">
+            </div>
+          </div>
+
+          <div class="col-sm">
+            <div class="form-group" style="text-align: right;">
+              <input type="submit" v-on:click="updatePassword" value="Update Password" class="btn btn-primary">
+            </div>
+          </div>
+          
+        </div>
+      </div> 
+    </div>
+    <Footer/>         
+    </div>
+</template>
+
+<script>
+import {fb, database} from '../../firebase';
+import Footer from '../Footer.vue';
+
+export default {
+  data(){
+    return {
+      account: {
+        email: null
+      },
+      email:null,
+      password:null,
+      confirmPassword:null,       
+    }
+  },
+  components: {
+    Footer
+  },
+  methods: {
+      fetchProfile() {
+        var user = fb.auth().currentUser;
+        this.account.email = user.email
+      },
+      updateProfile(){
+        var user = fb.auth().currentUser;
+
+        if (this.email !== null) {
+          user.updateEmail(this.email)
+            .catch(function(error) {
+              console.error(error);
+            });
+          database.collection("users").doc(user.uid).update({
+              email: this.email
+            })
+            .catch((error) => {
+                alert("Error updating document: " +  error);
+            });
+        }
+        
+        location.reload()
+        alert("Updated!")
+      },
+      updatePassword(){
+        var user = fb.auth().currentUser;
+
+        if (this.password !== this.confirmPassword) {
+            alert("Passwords do not match!")
+        } else if (this.password !== null) {
+          user.updatePassword(this.password)
+          .catch(function(error) {
+            console.error(error);
+          });
+          alert("Password updated!")
+        }
+      }
+  },
+  created() {
+    this.fetchProfile()
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 400px;
+  padding-bottom: 15px;
+}
+
+.btn-primary {
+    background-color: #688A75;
+    border-color: #688A75;
+    font-family: 'Work Sans';
+    color: white;
+}
+
+.btn-primary:hover {
+    background-color: #516e5c;
+    border-color: #516e5c;
+}
+</style>
