71469e65c35cffd2573c50fcf507c5a3ac529616
weelin26 <70046059+weelin26@users.noreply.github.com>
2021-04-19 18:58:11 +0800

    Added company sales analysis chart

---
+++ src/components/Company/SalesAnalysis.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div id="heading">
+      This section contains an overview of sales. We have calculated the sales
+      earned from each type of product.
+    </div>
+    <div class="chart">
+      <sales-chart></sales-chart>
+    </div>
+  </div>
+</template>
+
+<script>
+import SalesChart from "./SalesChart.js";
+
+export default {
+  components: {
+    SalesChart,
+  },
+  props: {
+    datacollection: {
+      type: Object,
+      default: null,
+    },
+    options: {
+      type: Object,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped>
+#heading {
+  font-family: "EB Garamond";
+  font-size: 25px;
+}
+</style>
